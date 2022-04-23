f13fb9fb09fb47cd1c04552871c7c798d2d5dca3
Layne Yarbrough <layne.yarbrough@student.allenisd.org>
2022-04-22 21:52:20 -0500

    Added backward movement

--- Character.swift
+++ Character.swift
@@ -7,8 +7,16 @@ class Character : RenderableEntity {
     var character : Image
     var character2 : Image
     var character3 : Image
+
+    var revChar1 : Image
+    var revChar2 : Image
+    var revChar3 : Image
+
+    var forward = true
     var frames : [Image]
     var index = 0
+    var revFrames : [Image]
+    var revIndex = 0
 
     var charX = 0
 
@@ -20,36 +28,58 @@ class Character : RenderableEntity {
         character = Image(sourceURL:imageURL)
         character2 = Image(sourceURL:URL(string:"https://codermerlin.com/users/layne-yarbrough/ISPImages/character2-removebg-preview.png")!)
         character3 = Image(sourceURL:URL(string:"https://codermerlin.com/users/layne-yarbrough/ISPImages/character3-removebg-preview.png")!)
+
+        revChar1 = Image(sourceURL:URL(string:"https://codermerlin.com/users/layne-yarbrough/ISPImages/character1back-removebg-preview.png")!)
+        revChar2 = Image(sourceURL:URL(string:"https://codermerlin.com/users/layne-yarbrough/ISPImages/character2back-removebg-preview.png")!)
+        revChar3 = Image(sourceURL:URL(string:"https://codermerlin.com/users/layne-yarbrough/ISPImages/character3back-removebg-preview.png")!)
         
         frames = []
         frames.append(character)
         frames.append(character2)
         frames.append(character3)
+
+        revFrames = []
+        revFrames.append(revChar1)
+        revFrames.append(revChar2)
+        revFrames.append(revChar3)
         super.init(name: "Character")
     }
 
     override func setup(canvasSize:Size, canvas:Canvas) {
-        canvas.setup(character)
-        canvas.setup(character2)
-        canvas.setup(character3)
+        canvas.setup(character,character2,character3,revChar1,revChar2,revChar3)
     }
 
     override func render(canvas:Canvas) {
         let clearRect = Rect(topLeft:Point(x:0, y:0), size:canvas.canvasSize!)
         let clearRectangle = Rectangle(rect:clearRect, fillMode:.clear)
         canvas.render(clearRectangle)
-        if index != 0 {
-            if frames[index].isReady {
-                canvas.render(frames[index])
+        if forward {
+            if index != 0 {
+                if frames[index].isReady {
+                    canvas.render(frames[index])
             
+                }
+                increaseIndex()
+            }else{
+                if frames[index].isReady {
+                    canvas.render(frames[index])
+                }
             }
-            increaseIndex()
+            index = 0
         }else{
-            if frames[index].isReady {
-                canvas.render(frames[index])
+            if revIndex != 0 {
+                if revFrames[revIndex].isReady {
+                    canvas.render(revFrames[revIndex])
+            
+                }
+                increaseRevIndex()
+            }else{
+                if revFrames[revIndex].isReady {
+                    canvas.render(revFrames[revIndex])
+                }
             }
+            revIndex = 0
         }
-        index = 0
         /*
         if frames[index].isReady {
             canvas.render(frames[index])
@@ -59,12 +89,18 @@ class Character : RenderableEntity {
     }
 
     func increaseIndex() {
-        if index == 0 {
-            index = 1
-        }else if index == 1 {
-            index = 2
-        }else{
+        if index >= 2 {
             index = 0
+        }else{
+            index += 1
+        }
+    }
+
+    func increaseRevIndex() {
+        if revIndex >= 2 {
+            revIndex = 0
+        }else{
+            revIndex += 1
         }
     }
 
@@ -75,6 +111,16 @@ class Character : RenderableEntity {
         frames[1].renderMode = .destinationPoint(Point(x:charX+19,y:30))
         frames[2].renderMode = .destinationPoint(Point(x:charX-2,y:30))
         charX += 10
+        forward = true
+    }
+
+    func moveBackward() {
+        increaseRevIndex()
         
+        revFrames[0].renderMode = .destinationPoint(Point(x:charX,y:30))
+        revFrames[1].renderMode = .destinationPoint(Point(x:charX+46,y:30))
+        revFrames[2].renderMode = .destinationPoint(Point(x:charX+55,y:30))
+        charX -= 10
+        forward = false
     }
 }
