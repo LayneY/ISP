c0eaa2f40d6f51c88db81306739537376a18e72d
Layne Yarbrough <layne.yarbrough@student.allenisd.org>
2022-04-27 10:04:01 -0500

    Changed else if to if

--- InteractionLayer.swift
+++ InteractionLayer.swift
@@ -18,7 +18,7 @@ class InteractionLayer : Layer, KeyDownHandler {
           super.init(name:"Interaction")
           insert(entity: character, at: .front)
 
-          insert(entity: platform, at: .front)
+          insert(entity: platform, at: .front)              
           // We insert our RenderableEntities in the constructor
 
       }
@@ -34,10 +34,11 @@ class InteractionLayer : Layer, KeyDownHandler {
       func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool) {
           if key == "d" {
               character.moveForward()
-              
+          
           }else if key == "a" {
               character.moveBackward()
-          }else if code == "Space" {
+          }
+          if code == "Space" {
               character.jump()
           }
           
